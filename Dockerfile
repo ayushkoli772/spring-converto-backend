e24edabde8c209c1d550a775b8452cccd7b33705
ayushkoli772 <113872531+ayushkoli772@users.noreply.github.com>
2024-10-14 19:28:26 +0530

    docker-changes

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
-FROM openjdk:17-jdk-alpine
+FROM openjdk:17-jdk
 
 LABEL maintainer="ayushkoli772@gmail.com"
 
 EXPOSE 8080
 
-ENTRYPOINT ["top", "-b"]
+WORKDIR /app
+
+COPY ./target/converto-backend-0.0.1-SNAPSHOT.jar /app
+
+CMD ["java", "-jar", "converto-backend-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/example/convertobackend/Controller/ConversionController.java
+++ src/main/java/com/example/convertobackend/Controller/ConversionController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.web.bind.annotation.*;
-@CrossOrigin(origins = "http://localhost:43949")
+@CrossOrigin(origins = "*")
 @RestController
 public class ConversionController {
 
@@ -17,16 +17,21 @@ public class ConversionController {
         return "<h1>Hello World";
     }
 
+    @GetMapping("/")
+    public String newDefaultROute(){
+        return "<h1>Hello World!";
+    }
+
     @Autowired
     private ConversionServices conversionService;
 
 
     @PostMapping("/convert")
     public String convertData(@RequestParam String prev, @RequestParam String newFormat, @RequestBody String inputCode) throws JSONException, JsonProcessingException {
-        System.out.println("Received Input: " + inputCode);
+//        System.out.println("Received Input: " + inputCode);
         String output = "";
         output = conversionService.convert( prev, newFormat, inputCode);
-        System.out.println(output);
+//        System.out.println(output);
         return output;
     }
 }
